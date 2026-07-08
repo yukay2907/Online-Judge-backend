f667d6e86d4c8effc3e06215abd0369d086506e8
Utkarsh Kashyap <116485473+yukay2907@users.noreply.github.com>
2026-07-09 00:48:31 +0530

    Implement centralized error handling architecture

--- controllers/problemController.js
+++ controllers/problemController.js
@@ -1,138 +1,84 @@
 const Problem = require("../models/Problem");
+const asyncHandler = require("../utils/asyncHandler");
+const ApiError = require("../utils/ApiError");
 
-const createProblem = async (req, res) => {
-  try {
-    const { title, description, difficulty, sampleInput, sampleOutput } =
-      req.body;
+const createProblem = asyncHandler(async (req, res) => {
+  const { title, description, difficulty, sampleInput, sampleOutput } =
+    req.body;
 
-    if (!(title && description && difficulty && sampleInput && sampleOutput)) {
-      return res.status(400).json({
-        success: false,
-        message: "Please send all the information.",
-      });
-    }
-    const existingProblem = await Problem.findOne({ title });
-    if (existingProblem) {
-      return res.status(400).json({
-        success: false,
-        message: "Problem with same name already exists.",
-      });
-    }
-
-    const problem = await Problem.create({
-      title,
-      description,
-      difficulty,
-      sampleInput,
-      sampleOutput,
-    });
-    return res.status(201).json({
-      success: true,
-      message: "Problem created successfully.",
-      problem,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
+  if (!(title && description && difficulty && sampleInput && sampleOutput)) {
+    throw new ApiError(400, "Please enter all the information.");
   }
-};
-
-const getProblems = async (req, res) => {
-  try {
-    const problems = await Problem.find();
-    return res.status(200).json({
-      success: true,
-      problems,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error.",
-    });
+  const existingProblem = await Problem.findOne({ title });
+  if (existingProblem) {
+    throw new ApiError(400, "Problem with same name already exists.");
   }
-};
 
-const getProblemById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const problem = await Problem.findById(id);
+  const problem = await Problem.create({
+    title,
+    description,
+    difficulty,
+    sampleInput,
+    sampleOutput,
+  });
+  return res.status(201).json({
+    success: true,
+    message: "Problem created successfully.",
+    problem,
+  });
+});
 
-    if (!problem) {
-      return res.status(404).json({
-        success: false,
-        message: "Problem not found.",
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      problem,
-    });
-  } catch (error) {
-    console.error(error);
+const getProblems = asyncHandler(async (req, res) => {
+  const problems = await Problem.find();
+  return res.status(200).json({
+    success: true,
+    problems,
+  });
+});
 
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error.",
-    });
+const getProblemById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const problem = await Problem.findById(id);
+
+  if (!problem) {
+    throw new ApiError(404, "Problem not found");
   }
-};
+  return res.status(200).json({
+    success: true,
+    problem,
+  });
+});
 
-const updateProblem = async (req, res) => {
-  try {
-    const { id } = req.params;
+const updateProblem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-    const updatedData = req.body;
+  const updatedData = req.body;
 
-    const problem = await Problem.findByIdAndUpdate(id, updatedData, {
-      new: true,
-      runValidators: true,
-    });
+  const problem = await Problem.findByIdAndUpdate(id, updatedData, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!problem) {
-      return res.status(404).json({
-        success: false,
-        message: "Problem not found.",
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      problem,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
+  if (!problem) {
+    throw new ApiError(404, "Problem not found.");
   }
-};
+  return res.status(200).json({
+    success: true,
+    problem,
+  });
+});
 
-const deleteProblem = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const problem = await Problem.findByIdAndDelete(id);
-    if (!problem) {
-      return res.status(404).json({
-        success: false,
-        message: "Problem does not exist.",
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      message: "Problem deleted successfully.",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error.",
-    });
+const deleteProblem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const problem = await Problem.findByIdAndDelete(id);
+  if (!problem) {
+    throw new ApiError(404, "Problem does not exist.");
   }
-};
+  return res.status(200).json({
+    success: true,
+    message: "Problem deleted successfully.",
+  });
+});
 
 module.exports = {
   createProblem,
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { DBConnection } = require("./database/db.js");
 
 const authRoutes = require("./routes/authRoutes");
 const problemRoutes = require("./routes/problemRoutes");
+const errorHandler = require("./middlewares/errorHandler");
 
 const cookieParser = require("cookie-parser");
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/problems", problemRoutes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on ${process.env.PORT}!`);
